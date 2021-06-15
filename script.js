5a2974b4e5eb0e28b8828262becc1a90450efeaf
Allyson-mkw <allysoneduardo04@gmail.com>
2021-06-14 21:43:45 -0300

    feat: removed unnecessary code

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const div = document.querySelector(".squareTable")
-
-div.addEventListener("mouseover", (e) => changeColor(e))
-div.addEventListener("mouseout", (e) => whiteOn(e))
-
 function changeColor(e) {
     let rdmColor = "#" + Math.random().toString(16).substr(2, 6)
     console.log(rdmColor)
