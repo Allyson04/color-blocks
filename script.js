e077d3853308f90f933d168ba8aa56a9678652f5
Allyson-mkw <allysoneduardo04@gmail.com>
2021-06-15 13:24:30 -0300

    feat: added creation of clock

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ function changeColor(e) {
 function whiteOn() {
     event.target.style.background = "#fff"
 }
+
+function createSquareBlocks(squareSide) {
+    templateBlock = `
+    <td onmouseover="changeColor()" onmouseout="whiteOn()" class="squareTable"></td>
+    `
+
+    table = document.createElement("table")
+    document.querySelector("main").appendChild(table)
+    
+    createTableRow(squareSide)
+}
+
+function createTableRow(squareSide) {
+    
+    for(i=0;i<squareSide;i++) {
+        tableRow = document.createElement("tr")
+    
+        document.querySelector("table").appendChild(createTableData(squareSide))
+    }
+}
+
+function createTableData(squareSide) {
+    for(y=0;y<squareSide;y++) {
+        td = document.createElement("td")
+        td.classList.add("squareTable")
+        tableRow.appendChild(td)
+    }
+
+    return tableRow
+}
+
+createSquareBlocks(5)
